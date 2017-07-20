13450b01c43ae24c928579ea7810a1a522374597
Samuel Lotz <samuel.lotz@salotz.info>
2017-07-20 19:30:56 -0400

    added option for giving a listing of the columns

--- peek/cli.py
+++ peek/cli.py
@@ -5,10 +5,21 @@ import pandas as pd
 import click
 
 @click.command()
+@click.option('--only-columns', '-C', is_flag=True, help="show only the columns for the table")
 @click.argument('csv', nargs=1, type=click.Path(exists=True))
-def peek(csv):
+def peek(only_columns, csv):
+
     df = pd.read_csv(csv, index_col=0)
-    sys.stdout.write(df.__str__())
+
+    # flags for only showing part of the data
+    if only_columns:
+        for column in df.columns.values:
+            sys.stdout.write(column.__str__() + "\n")
+        return None
+
+    sys.stdout.write(df.__str__() + "\n")
+
+    return None
 
 if __name__ == "__main__":
     peek()
